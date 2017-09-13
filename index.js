edff7eb76de3eea2996b3c9999cb7abfb4c582e7
John Tuttle <thejwt@gmail.com>
2017-09-13 05:16:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function theBeatlesPlay(musicians,instruments){
   var newArray = []
@@ -15,6 +21,9 @@ function johnLennonFacts(factsArray){
   while(i < factsArray){
     johnLennonFacts.push(factsArray[i] + "!!!")
     i++;
+    console.log(johnLennonFacts[i])
   }
   return johnLennonFacts
 }
+
+johnLennonFacts(facts)
