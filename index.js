066aba8751c3ee98a112bbae8507b379ea482cb0
John Tuttle <thejwt@gmail.com>
2017-09-13 05:11:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians,instruments){
+  var newArray = []
+  for(var i = 0; i < musicians.length; i++){
+    newArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return newArray
+}
+
+function johnLennonFacts(factsArray){
+  var i = 0;
+  var newArray = []
+  while(i < factsArray){
+    newArray.push(factsArray[i] + "!!!")
+    i++;
+  }
+  return newArray
+}
+
