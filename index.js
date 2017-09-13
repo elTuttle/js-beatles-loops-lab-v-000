7749fe921ac3e7fe3fd26af79b9770f9a0395aa6
John Tuttle <thejwt@gmail.com>
2017-09-13 05:13:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians,instruments){
 
 function johnLennonFacts(factsArray){
   var i = 0;
-  var newArray = Array
+  var newArray = []
   while(i < factsArray){
     newArray.push(factsArray[i] + "!!!")
     i++;
